80775e18e90d6bce8feed518a5256991e450b58d
unknown <ahmedfaez03@gmail.com>
2026-01-06 14:53:01 +0530

    Add Styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+import { use } from "react";
 
 function CreateArea(props) {
 
-
+  const [clicked, Setclicked] = useState(false);
   const [note, Setnote] = useState({
     id: 0,
     title: "",
@@ -22,19 +26,27 @@ function CreateArea(props) {
 
   }
 
+  function handleClick(){
+    Setclicked(true);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" onChange={handleChange} value={note.title}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} value={note.content}/>
-        <button type="button" onClick={() => {
-          Setnote((prev) => {return {
-            id: prev.id + 1,
-            title: "",
-            content: "",
-          }});
-          return props.click(note);
-        }}>Add</button>
+      <form className="create-note">
+        {clicked && <input name="title" placeholder="Title" onChange={handleChange} value={note.title}/>}
+        <textarea name="content" placeholder="Take a note..." rows={clicked ? "3" : "1"}  onChange={handleChange} onClick={handleClick} value={note.content}/>
+        <Zoom in = {clicked ? "true" : null}>
+          <Fab type="button" onClick={() => {
+              Setnote((prev) => {return {
+                id: prev.id + 1,
+                title: "",
+                content: "",
+              }});
+              return props.click(note);
+            }}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
 function Note(props) {
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button type = "button" onClick={() => props.delete(props.id)}>DELETE</button>
+      <Zoom in = "true">
+      <Fab type = "button" onClick={() => props.delete(props.id)}>
+        <DeleteIcon />
+      </Fab></Zoom>
     </div>
   );
 }
